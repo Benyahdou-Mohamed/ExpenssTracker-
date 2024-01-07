0a3506b82af3700c40b842b1e4860d5f3312a112
unknown <benyahdou.moh29@hotmail.com>
2024-01-07 22:17:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {Header} from './Components/Header'
+import {Balance} from './Components/Balance'
+import {Income} from './Components/Income'
+import {TransactionList} from './Components/TransactionList'
+import {AddTransaction} from './Components/AddTransaction'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="expenss-container">
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <Income/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
